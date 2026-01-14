1cae3ebdc9ab7f518205d0a969e7d11d6097fc86
kashefinterior-tech <kashef.interior@gmail.com>
2026-01-14 16:26:47 -0500

    added car.php

--- 02/index.php
+++ 02/index.php
@@ -1,9 +1,33 @@
+<?php
+declare (strict_types=1);
+
 //1. Set Up & Start 
 
 
-//2. Code Commenting 
+//2. Code Commenting
+//inline comment --
+$firstName ="Melika"; //string
+$lastName="Kashef";//string
+$age=40;//int
+$isInstructor=true; //boolean;
 
+echo "<p>Hello there , my name is ".$firstName."".$lastName."</p>";
 
+if ($isInstructor){
+    echo"<p> I am your teacher </p>";
+}
+else{
+    echo "<p> Opps, teach didn't show! </p>";
+}
+
+$num1=1;
+$num2=10;
+
+function add(int $num1, int $num2) {
+    return $num1+$num2;
+}
+
+echo "<p>" .add($num1,$num2). "</p>";
 //3. Variables, Data Tyoes, Concatenation, Conditional Statements & Echo
 
 
@@ -14,3 +38,4 @@
 
 
 //6. OOP with PHP 
+
