b4edb0a7468a7a36d92c7e75339d73f9caf89706
kashefinterior-tech <kashef.interior@gmail.com>
2026-01-14 21:51:27 -0500

    added class

--- assignments/labs/lab_one/car.php
+++ assignments/labs/lab_one/car.php
@@ -1,17 +1,9 @@
 <?php
+class Car{
+    public $make;
+    public $model;
+    public $year;
 
-$carMake="Toyota";
-$carModel="RAV4";
-$carYear="2026";
-$carGeneration="sixth-generation";
-
-echo "<p> an all-new, ".$carGeneration." ".$carYear." ".$carMake." ".$carModel." is launching for the 2026 model year, featuring a fully hybrid lineup, redesigned styling, updated technology </p>";
-
-
-try{
-    
 }
 
-catch{
-    
-}
+
