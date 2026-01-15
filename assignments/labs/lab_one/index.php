33b9d9a00da69b890364d5e10b2a65dbb603f8bb
kashefinterior-tech <kashef.interior@gmail.com>
2026-01-14 21:56:56 -0500

    require_once "car.php"; require_once "connect.php";

--- assignments/labs/lab_one/index.php
+++ assignments/labs/lab_one/index.php
@@ -1,4 +1,7 @@
 <?php 
+require_once "car.php";
+require_once "connect.php";
 require "header.php"; 
 echo "<p> Follow the instructions outlined in instructions.txt to complete this lab. Good luck & have fun!😀 </p>";
-require "footer.php"; 
+require "footer.php";
+
