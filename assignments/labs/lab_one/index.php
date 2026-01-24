569fed5d51269a9652310dad229f5a45e62d542c
Melika Kashef <kashef.interior@gmail.com>
2026-01-24 11:01:07 -0500

    Add car information display and update footer inclusion

--- assignments/labs/lab_one/index.php
+++ assignments/labs/lab_one/index.php
@@ -1,4 +1,16 @@
 <?php 
+require_once "car.php";
+require_once "connect.php";
+
 require "header.php"; 
+$car = new Car();
+$car->make = "Toyota";
+$car->model = "RAV4";
+$car->year = 2026;
+
+echo "<p>" . $car->getInfo() . "</p>";
+echo "<p> 🚗 </p>";
+
 echo "<p> Follow the instructions outlined in instructions.txt to complete this lab. Good luck & have fun!😀 </p>";
-require "footer.php"; 
+require "footer.php";
+ 
