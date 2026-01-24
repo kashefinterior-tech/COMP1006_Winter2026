d40eadf910efa0e8b7e13010288749d00a883fd4
Melika Kashef <kashef.interior@gmail.com>
2026-01-24 11:00:14 -0500

    Add Car class with properties and getInfo method

--- assignments/labs/lab_one/car.php
+++ assignments/labs/lab_one/car.php
@@ -1,1 +1,14 @@
+<?php
+//class car
+class Car{
+    public $make;
+    public $model;
+    public $year;
+
+    public function getInfo(){
+        return $this->year ."". $this->make ."". $this->model;
+    }
+
+}
+
 
