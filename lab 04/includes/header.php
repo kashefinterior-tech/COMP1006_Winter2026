21212619a42e94b3d15b19b980edfea9f8490972
kashefinterior-tech <kashef.interior@gmail.com>
2026-01-31 18:20:07 -0500

    save

--- lab 04/includes/header.php
+++ lab 04/includes/header.php
@@ -3,12 +3,12 @@
     <head>
         <meta charset="UTF-8">
         <title>Bake it Till you make it</title>
-        <link rel="stylesheet" href="css/main.css">
+        <link rel="stylesheet" href="../css/main.css">
     </head>
     <body>
         <header>
             <h1>
-                <img src="assets/bitumi.png" alt="Bake it Till you make it">
+                <img src="../assets/bitumi.png" alt="Bake it Till you make it">
             </h1>
         </header>
 
