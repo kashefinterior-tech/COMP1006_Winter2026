007b9f76b257f8348b14ea5e620a9e22292c4dd2
kashefinterior-tech <kashef.interior@gmail.com>
2026-01-31 15:42:22 -0500

    head.php

---
+++ 04/includes/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Bake it Till you make it</title>
+        <link rel="stylesheet" href="css/main.css">
+    </head>
+    <body>
+        <header>
+            <h1>
+                <img src="" alt="">
+            </h1>
+        </header>
+
+        <main>
+
+        
+        </main>
+    </body>
+</html>
