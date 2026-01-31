6eb5bfe0abeb350db86d613593999b8fb551ff16
kashefinterior-tech <kashef.interior@gmail.com>
2026-01-31 15:56:45 -0500

    index.php

---
+++ 04/index.php
@@ -0,0 +1,25 @@
+<?php include "header.php"; ?>
+<h2>Contact our Bakery</h2>
+
+<form action="process.php" method="post">
+    <fieldset>
+        <legend>Your Information</legend>
+
+        <label for="first_name">First name</label>
+        <input type="text" name="first_name" id="first_name" required>
+
+        <label for="last_name">last name</label>
+        <input type="text" name="last_name" id="last_name" required>
+
+        <label for="email">Email</label>
+        <input type="email" name="email" id="email" required>
+
+        <label for="message">Message</label>
+        <textarea name="message" id="message" required></textarea>
+    </fieldset>
+
+    <button type="submit">Send Message</button>
+</form>
+
+<?php include "footer.php";
+?>ß
