28b5961a38b34bafa6710d1150730297e9c9a060
kashefinterior-tech <kashef.interior@gmail.com>
2026-01-31 18:33:08 -0500

    w

--- lab 04/includes/header.php
+++ lab 04/includes/header.php
@@ -1,17 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <title>Bake it Till you make it</title>
-        <link rel="stylesheet" href="../css/main.css">
-    </head>
-    <body>
-        <header>
-            <h1>
-                <img src="../assets/bitumi.png" alt="Bake it Till you make it">
-            </h1>
-        </header>
-
-        <main>
-
+<head>
+    <meta charset="UTF-8">
+    <title>Bake it Till you make it</title>
+    <link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+<header>
+    <h1>
+        <img src="assets/bitumi.png" alt="Bake it Till you make it">
+    </h1>
+</header>
 
