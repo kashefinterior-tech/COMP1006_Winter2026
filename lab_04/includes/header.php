e3e54fc4bd0ef74cbe4ca791b00c11dcae49d676
kashefinterior-tech <kashef.interior@gmail.com>
2026-01-31 20:57:01 -0500

    commit

--- lab_04/includes/header.php
+++ lab_04/includes/header.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Bake It Till You Make It</title>
-  <link rel="stylesheet" href="styles/main.css">
+  <link rel="stylesheet" href="css/main.css">
 </head>
 
 <body>
