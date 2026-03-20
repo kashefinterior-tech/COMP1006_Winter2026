ae1a0f0b9584c6fa4d05240a5b79da8ef9842ab4
Melika Kashef <kashef.interior@gmail.com>
2026-03-20 01:27:23 -0400

    Add files via upload

---
+++ lab5/index.php
@@ -0,0 +1,31 @@
+/*
+Name: Melika Kashef
+Student Number: 200652992
+Course: COMP1006
+Lab: Lab Five - Image Upload
+Date: March 2026
+Description: This program allows users to upload an image and displays it. 
+*/
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, intial-scale=1.0">
+        <title>Lab-five-upload profile picture</title>
+    </head>
+    <body>
+
+    <h1>upload profile picture</h1>
+        <form action="upload.php" method="post" enctype="multipart/form-data">
+            <label for="profileImage">Pick an Image:</label>
+            <input type="file" name="profileImage" id="profileImage" accept="image/*" required>
+
+            <br><br>
+
+            <button type="submit" name="submit">upload Image</button>
+        </form>
+
+
+    </body>
+</html>
---
+++ lab5/upload.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>upload result</title>
+    </head>
+    <body>
+
+    <h1>upload result</h1>
+
+    <?php
+    if(isset($_POST['submit'])){
+        if(isset($_FILES['profileImage']) && $_FILES['profileImage']['error']===0){
+
+            $fileName=$_FILES['profileImage']['name'];
+            $tempName=$_FILES['profileImage']['tmp_name'];
+            $fileSize=$_FILES['profileImage']['size'];
+
+            $uploadFolder="uploads/";
+            $targetFile=$uploadFolder.basename($fileName);
+
+            $imageFileType=strtolower(pathinfo($targetFile, PATHINFO_EXTENSION));
+            $allowedTypes=["jpg","jpeg","png","gif","webp"];
+
+            if(in_array($imageFileType, $allowedTypes)){
+
+                if(move_uploaded_file($tempName, $targetFile)){
+                echo"<p> image uploaded successfully!</p>";
+                echo"<p>file name:" .htmlspecialchars($fileName)."</p>";
+                echo"<img src='$targetFile' alt='Uploaded Image' width='300'>";
+            }else{
+                echo"<p>sorry, there is the problem.</p>";
+            }
+            }else{
+                echo"<p> only jpg, jpeg, png, Gif, and WEBP files are allowed.</p>;";
+            }
+            }else{
+                echo"<p> no file uploaded or there is an error.</p>";
+            }
+            }else{
+                echo"<p> please upload an image using form.</p>";
+            }
+          
+
+    
+    ?>
+    <br><br>
+
+    <a href="index.php">go back</a>
+
+    </body>
+</html>
